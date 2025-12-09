05f808d3d9b15faa3cdca9bad34ba6c8d048eadf
Jordan Low <jordanlow.jinyi@gmail.com>
2025-12-10 00:03:52 +0800

    Add paintings

--- Assets/Assets/Scripts/SampleScannable.cs
+++ Assets/Assets/Scripts/SampleScannable.cs
@@ -5,9 +5,16 @@ public class SampleScannable : MonoBehaviour, Scannable
 	public DialogueManager dialogueManager;
     public Dialogue dialogue;
 	
+	[SerializeField] private Material unscanned;
+	[SerializeField] private Material scanned;
+	[SerializeField] private MeshRenderer mat;
+	
     public void Scan() {
 		dialogueManager.StartDialogue(dialogue);
         DialogueManager.onDialogueEnd += OnDialogueEnd;
+		Material[] mats = mat.materials;
+		mats[1] = scanned;
+		mat.materials = mats;
 	}
 	
 	public void OnDialogueEnd(Dialogue dialogueEnded) {
